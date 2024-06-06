ee97f543ef0b364814ee7569a3420cea46169a1e
assma-codes <assma.hm89@gmail.com>
2024-06-06 08:34:37 -0700

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
-
 def number_of_subscribers(subreddit):
-    """Queries the Reddit API and returns the number of subscribers
-    to the subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
@@ -13,5 +10,4 @@ def number_of_subscribers(subreddit):
                             allow_redirects=False)
     if sub_info.status_code >= 300:
         return 0
-
     return sub_info.json().get("data").get("subscribers")
