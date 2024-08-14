1f1b1e387589cbbdfc8087e6b5cf38ee85843cc2
assma-codes <assma.hm89@gmail.com>
2024-08-13 19:42:02 -0700

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
 """
 Function that queries the Reddit API and returns the number of subscribers
+(not active users, total subscribers) for a given subreddit.
+If an invalid subreddit is given, the function should return 0
 """
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
-    Function  queries  Reddit API
+    Function that queries the Reddit API
     - If not a valid subreddit, return 0.
     """
     req = requests.get(
