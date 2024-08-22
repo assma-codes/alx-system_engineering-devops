aa8d4afee4f097a77ed493f8f3d7e3e4dc523ab7
assma-codes <assma.hm89@gmail.com>
2024-08-22 07:08:36 +0000

    task1

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,10 @@
-hange the OS configuration so that it is possible to login with the
-# holberton user and open a file without any error message.
+# Puppet manifest to increase file limits of user
+exec { 'file limit config':
+  command => "sed -i s/'nofile 5'/'nofile 100'/g /etc/security/limits.conf",
+  path    => '/bin'
+}
 
-exec {'OS security config':
-  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
-  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
-}i
+exec { 'file limit config_2':
+  command => "sed -i s/'nofile 4'/'nofile 100'/g /etc/security/limits.conf",
+  path    => '/bin'
+}
